bbf959d907df684084f0c2e5cb318a2b46b24f7a
DMZ <nguyen.dmz@gmail.com>
2025-03-09 19:20:23 +0700

    Fix Auth flow

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -73,7 +73,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         const { error } = await supabase.auth.signInWithOAuth({
           provider: "google",
           options: {
-            redirectTo: `${window.location.origin}/auth`,
+            redirectTo: `${window.location.origin}`,
           },
         });
         
@@ -82,7 +82,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         const { error } = await supabase.auth.signInWithOtp({
           email: options.email,
           options: {
-            emailRedirectTo: `${window.location.origin}/auth`,
+            emailRedirectTo: `${window.location.origin}`,
           },
         });
         
